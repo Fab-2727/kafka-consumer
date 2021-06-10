be332a5e704c5d993c8054fd34ee11a3b01c9c29
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-06-09 22:22:03 -0300

    Add logger

--- src/main/java/kafkafirst/service/KafkaConsumerService.java
+++ src/main/java/kafkafirst/service/KafkaConsumerService.java
@@ -9,17 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class KafkaConsumerService {
 
-	/*
-	 @KafkaListener(containerFactory = "myKafkaListenerContainerFactory", topics = "myTopic")
-	 public void process(String msg, @Header("kafka_partition") int partition) {
-	 	// process incoming message
-	 }
-	 */
 	@KafkaListener(topics = "${spring.kafka.listener.topic}", groupId = "${spring.kafka.consumer.group-id}"
 			, autoStartup = "${spring.kafka.listener.auto-start}")
 	public void process (String msg) {
-		System.out.println(msg);
-		log.debug("[SERVICE]: message received" +"["+msg+"]");
+		log.debug("[service]: message from topic" +"[ "+msg+" ]");
 	}
 	
 }
